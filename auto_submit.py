b8a5a98cab045fde344ee21fcac191698ead31e3
muffin1000 <t.ohtani1028@gmail.com>
2023-02-12 11:38:43 +0900

    chenge loop time

--- auto_submit.py
+++ auto_submit.py
@@ -1,27 +1,30 @@
 from setting import *
 from lib.db import DB
 from lib.leber import Submit
-@tasks.loop(seconds=10)
+from datetime import datetime
+
+@tasks.loop(seconds=60)
 @client.event
 async def auto_submit():
-    db = DB()
-    for item in db.get_all_data():
-        submit = Submit(*item[:4])
-        if item[-1]: submit.exec()
-        if submit.error is None:
-            user = await client.fetch_user(item[1])
-            embed = discord.Embed(
-                title='成功',
-                description=submit.norm,
-                color=discord.Color.green()
-            )
-            await user.send(embed=embed)
-        else:
-            user = await client.fetch_user(item[1])
-            embed = discord.Embed(
-                title='エラー',
-                description=submit.error,
-                color=discord.Color.red()
-            )
-            await user.send(embed=embed)
-    db.init_bool()
+    if datetime.now().strftime('%H:%M') == '07:25':            
+        db = DB()
+        for item in db.get_all_data():
+            submit = Submit(*item[:4])
+            if item[-1]: submit.exec()
+            if submit.error is None:
+                user = await client.fetch_user(item[1])
+                embed = discord.Embed(
+                    title='成功',
+                    description=submit.norm,
+                    color=discord.Color.green()
+                )
+                await user.send(embed=embed)
+            else:
+                user = await client.fetch_user(item[1])
+                embed = discord.Embed(
+                    title='エラー',
+                    description=submit.error,
+                    color=discord.Color.red()
+                )
+                await user.send(embed=embed)
+        db.init_bool()
