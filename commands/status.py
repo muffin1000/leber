0322c481d727a29c496f41975f6ddd8743ed4e11
muffin1000 <t.ohtani1028@gmail.com>
2023-02-24 21:04:23 +0900

    fix status

--- commands/status.py
+++ commands/status.py
@@ -9,7 +9,7 @@ async def status(interaction: discord.Interaction):
     print(f'status command from {interaction.user.id}')
     embed = discord.Embed(
         title='Status',
-        description=f'{Status.load.value}\n{Status.server_status.value}',
+        description=f'BotサーバのCPU使用率: {Status.load.value}\nLEBERの状況{Status.server_status.value}',
         color=discord.Colour.blue()
     )
     await interaction.response.send_message(embed=embed)
--- lib/status.py
+++ lib/status.py
@@ -2,14 +2,17 @@ import psutil
 from enum import Enum
 import requests
 
-def get_server_status():
-    url = 'https://api.leber11.com/v9//users/sign_in'
-    re = requests.post(url)
-    return re.status_code
 
-class Status(Enum):
-    load = f'{psutil.cpu_percent(interval=1)}%'
-    server_status = get_server_status()
+
+class Status:
+    def __init__(self) -> None:
+        self.load = f'{psutil.cpu_percent(interval=1)}%'
+        self.server_status = self.get_server_status()
+
+    def get_server_status():
+        url = 'https://api.leber11.com/v9//users/sign_in'
+        re = requests.post(url)
+        return re.status_code
 
 
 print(Status.server_status.value)
